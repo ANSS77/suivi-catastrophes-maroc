75b24954bd06f024e06fb8edb65fd7b9775cde30
Anssem Hafid <hafidanssem05@gmail.com>
2026-04-23 13:19:03 +0100

    feat(users): add auth services

--- backend/apps/users/services/__init__.py
+++ backend/apps/users/services/__init__.py
@@ -1,1 +1,18 @@
- 
+from apps.users.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def authenticate_user(input_email, input_password):
+    user = User.objects(email=input_email).first() # chercher en DB
+    if user and user.check_password(input_password): # vérifier password hashé
+        return user
+    return None
+
+
+def generate_tokens(user):
+    token = RefreshToken()
+    token['user_id'] = str(user.id)
+    return {
+        'access': str(token.access_token),
+        'refresh': str(token)
+    }
