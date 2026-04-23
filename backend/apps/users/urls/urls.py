92a1977da679b5d5aaaa11f7b1f1df87f9793097
Anssem Hafid <hafidanssem05@gmail.com>
2026-04-23 13:19:32 +0100

    feat(users): add auth urls

--- backend/apps/users/urls/urls.py
+++ backend/apps/users/urls/urls.py
@@ -1,1 +1,10 @@
- 
+from django.urls import path
+from apps.users.views import RegisterView, LoginView, LogoutView
+
+
+# APIView → utiliser .as_view() pour convertir la classe en vue
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='auth-register'),
+    path('auth/login/',    LoginView.as_view(),    name='auth-login'),
+    path('auth/logout/',   LogoutView.as_view(),   name='auth-logout'),
+]
