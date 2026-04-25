17b53fdac4d61ee5ad3773cfa66d7aa5398ca3da
ismailouchraa <ismail.ouchraa.70@edu.uiz.ac.ma>
2026-04-25 14:25:12 +0100

    feat(predictions): add serializers, views, urls

--- backend/apps/predictions/serializers/__init__.py
+++ backend/apps/predictions/serializers/__init__.py
@@ -1,1 +1,11 @@
  
+from rest_framework import serializers
+
+
+class PredictionSerializer(serializers.Serializer):
+    score = serializers.FloatField()
+    region = serializers.CharField()
+    phenomenon = serializers.ChoiceField(
+        choices=['earthquake', 'flood', 'wildfire']
+    )
+    date = serializers.DateTimeField(read_only=True)
