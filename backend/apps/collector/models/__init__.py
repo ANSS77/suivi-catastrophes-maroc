4c113dd3328fa8544cf9bf705e1ef4a1851c4ef8
ismailouchraa <ismail.ouchraa.70@edu.uiz.ac.ma>
2026-04-25 14:24:28 +0100

    feat(collector): add Collector model

--- backend/apps/collector/models/__init__.py
+++ backend/apps/collector/models/__init__.py
@@ -1,1 +1,13 @@
  
+from mongoengine import Document, StringField, DateTimeField
+
+
+class Collector(Document):
+    schedule = StringField()
+    lastRun = DateTimeField()
+    status = StringField(
+        choices=['running', 'idle', 'error'],
+        default='idle'
+    )
+
+    meta = {'collection': 'collectors'}
