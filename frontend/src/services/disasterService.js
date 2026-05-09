270205f9880a1bb69f8f7309df9d5058f6adfd24
Amine haifi <haifimohamedamine@gmail.com>
2026-05-09 14:41:29 +0100

    feat: implement disasterService with getDisasters, filterDisasters, getDisasterById

--- frontend/src/services/disasterService.js
+++ frontend/src/services/disasterService.js
@@ -1,1 +1,19 @@
- 
+ import api from '../api/axios';
+
+// GET tous les disasters
+export const getDisasters = async () => {
+  const response = await api.get('/disasters/');
+  return response.data;
+};
+
+// GET disasters filtrés par type
+export const filterDisasters = async (type) => {
+  const response = await api.get(`/disasters/filter/?type=${type}`);
+  return response.data;
+};
+
+// GET un disaster par id
+export const getDisasterById = async (id) => {
+  const response = await api.get(`/disasters/${id}/`);
+  return response.data;
+};
